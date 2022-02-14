6fbe3f3cd4bd5257a59bb111b7b9d1894b9f8d9c
AudriusBukis <audriusbukis@gmail.com>
2022-02-14 10:51:07 +0200

    program upload

---
+++ Lesson12/lesson12.Models/TVPrograme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson12.Models
+{
+    public class TVPrograme
+    {
+        public List<TVShow> TVShows { get; set; }
+
+        public TVPrograme()
+        {
+            TVShows = new List<TVShow>();
+        }
+       
+    }
+}
---
+++ Lesson12/lesson12.Models/TVShow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson12.Models
+{
+    public class TVShow
+    {
+        public int Id { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+       // public List<string> Test = new List<string>(); galimas ir tesioginis list deklaravimas
+        public TVShow()
+        {
+
+        }
+        public TVShow(int id, TimeSpan duration, string title, string description, DateTime startTime)
+        {
+            Id = id;
+            Duration = duration;
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            StartTime = startTime;
+        }
+
+        public override string ToString()
+        {
+            return $"Show Id:{Id} title of show:{Title} - Description ({Description}) Duration: {Duration}, Start time - {StartTime}";
+        }
+    }
+}
